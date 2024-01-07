b8817427f7a14719c53c6432d6878235ac282aa8
ChihYang218 <chihyang218@gmail.com>
2024-01-07 22:52:53 +0800

    建立政黨候選人小百科頁面，並設置好 router

--- Node-Vote2024/routes/index.js
+++ Node-Vote2024/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '首頁' });
 });
 
+router.get('/KMTwiki', function(req, res, next) {
+  res.render('KMTwiki', { title: '國民黨正副總統候選人' });
+});
+
+router.get('/DPPwiki', function(req, res, next) {
+  res.render('DPPwiki', { title: '民進黨正副總統候選人' });
+});
+
+router.get('/TPPwiki', function(req, res, next) {
+  res.render('TPPwiki', { title: '民眾黨正副總統候選人' });
+});
+
 module.exports = router;
